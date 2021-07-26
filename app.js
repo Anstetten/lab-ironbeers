d90c5813f7f8cfdf387d39c1331ebb00ad6cd4d1
Anstetten <h.zsola@gmail.com>
2021-07-26 15:38:13 +0200

    Iteration N°3 done

--- app.js
+++ app.js
@@ -22,4 +22,17 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+
+punkAPI.getBeers()
+  .then((beersResponse)=>{
+   
+    res.render("beers.hbs",{
+      beers:beersResponse,
+    })
+  })
+  .catch(error=>console.log(error))
+
+});
+
 app.listen(3000, () => console.log('http://localhost:3000'));
